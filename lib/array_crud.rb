91c336f3032749f59bbc55b212f23598ecb6d85e
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-24 22:00:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,8 @@ def create_an_empty_array
 end
 
 def create_an_array
-  one_array = new.A
+  one_array = Array.new 
+  one_array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
